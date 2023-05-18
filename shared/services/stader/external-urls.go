7a96ad2a0902af551e6581bf22f21eefdefc2c26
Bharath Vedartham <vedabharath12345@gmail.com>
2023-05-18 11:29:16 +0530

    fix public key url

--- shared/services/stader/external-urls.go
+++ shared/services/stader/external-urls.go
@@ -2,5 +2,5 @@ package stader
 
 const PreSignSendApi = "https://stage-ethx-offchain.staderlabs.click/presign"
 const PreSignCheckApi = "https://stage-ethx-offchain.staderlabs.click/msgSubmitted"
-const PublicKeyApi = "https://stage-ethx-offchain.staderlabs.click/prod/publicKey"
+const PublicKeyApi = "https://stage-ethx-offchain.staderlabs.click/publicKey"
 const MerkleProofAggregateGetterApi = "https://stage-ethx-offchain.staderlabs.click/prod/merklesForElRewards/proofs/%s"
