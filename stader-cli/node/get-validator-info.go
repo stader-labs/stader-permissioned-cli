2cdcd5f496ce8eb06a1fe1b88fbf421b7db79c73
Bharath Vedartham <vedabharath12345@gmail.com>
2023-05-19 14:49:48 +0530

    only show cl rewards if it is greater then 0

--- stader-cli/node/get-validator-info.go
+++ stader-cli/node/get-validator-info.go
@@ -3,6 +3,7 @@ package node
 import (
 	"fmt"
 	"github.com/stader-labs/stader-node/shared/services/stader"
+	"github.com/stader-labs/stader-node/shared/utils/log"
 	"github.com/stader-labs/stader-node/shared/utils/math"
 	"github.com/stader-labs/stader-node/stader-lib/types"
 	"github.com/stader-labs/stader-node/stader-lib/utils/eth"
@@ -26,11 +27,14 @@ func GetValidatorInfo(c *cli.Context, validatorPubKey types.ValidatorPubkey) err
 	fmt.Printf("Validator PubKey: %s\n", types.BytesToValidatorPubkey(response.ValidatorInfo.Pubkey).String())
 	fmt.Printf("Validator WithdrawVault: %s\n", response.ValidatorInfo.WithdrawVaultAddress.String())
 	fmt.Printf("Validator Status: %s\n", response.DisplayStatus)
-	fmt.Printf("Validator ClRewards: %.6f\n", math.RoundDown(eth.WeiToEth(response.ClRewards), 18))
-	if response.ValidatorInfo.DepositBlock.Cmp(big.NewInt(0)) != 0 {
+	if response.ClRewards.Cmp(big.NewInt(0)) > 0 {
+		fmt.Printf("Validator ClRewards: %.6f\n", math.RoundDown(eth.WeiToEth(response.ClRewards), 18))
+		fmt.Printf("To claim skimmed rewards use the %sstader-permissioned-cli node claim-cl-rewards %s command\n\n", log.ColorGreen, log.ColorReset)
+	}
+	if response.ValidatorInfo.DepositBlock.Cmp(big.NewInt(0)) > 0 {
 		fmt.Printf("Validator DepositBlock: %d\n", response.ValidatorInfo.DepositBlock)
 	}
-	if response.ValidatorInfo.WithdrawnBlock.Cmp(big.NewInt(0)) != 0 {
+	if response.ValidatorInfo.WithdrawnBlock.Cmp(big.NewInt(0)) > 0 {
 		fmt.Printf("Validator WithdrawnBlock: %d\n", response.ValidatorInfo.WithdrawnBlock)
 	}
 
