60784e07c19948589446c2fef88b0b661f842f41
Bharath Vedartham <vedabharath12345@gmail.com>
2023-05-22 14:05:18 +0530

    update build scripts

--- daemon-build.sh
+++ daemon-build.sh
@@ -21,7 +21,7 @@
 # Get the platform type and run the build script if possible
 PLATFORM=$(uname -s)
 if [ "$PLATFORM" = "Linux" ]; then
-    docker run --rm -v $PWD:/stader-node staderdev/stader-node-builder:latest /stader-node/stader/build.sh
+    docker run --rm -v $PWD:/staderlabs staderlabs/stader-permissioned-builder:latest /stader-node/stader/build.sh
 else
     echo "Platform ${PLATFORM} is not supported by this script, please build the daemon manually."
     exit 1
