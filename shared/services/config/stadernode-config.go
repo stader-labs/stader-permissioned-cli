b842dc69d10fe3dbb7460a8f91aacf01bf77c88d
Bharath Vedartham <vedabharath12345@gmail.com>
2023-05-27 20:27:10 +0530

    update contracts

--- shared/services/config/stadernode-config.go
+++ shared/services/config/stadernode-config.go
@@ -193,16 +193,16 @@ func NewStadernodeConfig(cfg *StaderConfig) *StaderNodeConfig {
 		},
 
 		ethxTokenAddress: map[config.Network]string{
-			config.Network_Prater:   "0x38DE8Df722B4032Cc6987F00bCA0d9B37d9F9438",
-			config.Network_Devnet:   "0x38DE8Df722B4032Cc6987F00bCA0d9B37d9F9438",
-			config.Network_Mainnet:  "0x38DE8Df722B4032Cc6987F00bCA0d9B37d9F9438",
+			config.Network_Prater:   "0x69A8De09BcbF673AF5927a587936F7024B04d187",
+			config.Network_Devnet:   "0x69A8De09BcbF673AF5927a587936F7024B04d187",
+			config.Network_Mainnet:  "0x69A8De09BcbF673AF5927a587936F7024B04d187",
 			config.Network_Zhejiang: "0x90Da3CA75532A17ca38440a32595F036ecE46E85",
 		},
 
 		staderConfigAddress: map[config.Network]string{
-			config.Network_Prater:   "0x8eF9036E524ce6340eF71844C29508C26Fbbe478",
-			config.Network_Devnet:   "0x8eF9036E524ce6340eF71844C29508C26Fbbe478",
-			config.Network_Mainnet:  "0x8eF9036E524ce6340eF71844C29508C26Fbbe478",
+			config.Network_Prater:   "0x749Ed651c4F41E0D705960e815A58815ffFd3afe",
+			config.Network_Devnet:   "0x749Ed651c4F41E0D705960e815A58815ffFd3afe",
+			config.Network_Mainnet:  "0x749Ed651c4F41E0D705960e815A58815ffFd3afe",
 			config.Network_Zhejiang: "0x90Da3CA75532A17ca38440a32595F036ecE46E85",
 		},
 
