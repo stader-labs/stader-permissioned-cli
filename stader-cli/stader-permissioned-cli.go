74352f51edfaf882a2fe44d6d130a9e427e38abf
Bharath Vedartham <vedabharath12345@gmail.com>
2023-06-19 18:27:42 +0530

    update license

--- stader-cli/stader-permissioned-cli.go
+++ stader-cli/stader-permissioned-cli.go
@@ -51,14 +51,10 @@ func main() {
 	app := cli.NewApp()
 
 	// Set application info
-	app.Name = "Stader permissioned node beta"
+	app.Name = "Stader permissioned node"
 	app.Usage = "Stader Permissioned Node CLI"
 	app.Version = shared.StaderVersion
-	app.Description = `This is a beta release solely intended for testing purposes. This release is only meant for private beta participants who have expressed interest in participating in our beta program through our discord servers. Any other usage of this release is unauthorized and if you are in possession of an unauthorized copy you should delete the same and inform admin@staderlabs.com immediately.
-
-This version of the release is a pre production version, undergoing security audit and may contain bugs/ vulnerabilities and errors which may or may not be known to the Stader team. By using this release you accept that you have the necessary technical know how to independently assess risks associated with the usage of pre production software and agree to hold Stader Labs, and Stakeinfra Technologies Inc. indemnified of any and all damages that may arise from the usage of this release.
-
-For clarity, this version of the software is not meant for public consumption and may lead to technological and/ or financial losses. This version of the software is provided as is and only for testing purposes and should not be redistributed without the express consent of Stakeinfra Technologies Inc.`
+	app.Description = ``
 
 	app.Copyright = "(c) 2023 Rocket Pool Pty Ltd. Modified under GNU GPL v3. [1.0.0].\n   (c) 2023 Stakeinfra Technologies Inc."
 
